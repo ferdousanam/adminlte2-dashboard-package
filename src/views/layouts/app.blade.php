7e7c46891ba8b5de0b8bbce8d696dd644d15002a
Ferdous Anam <ferdous.anam@gmail.com>
2020-02-22 22:51:49 +0600

    Layout organized

--- src/views/layouts/app.blade.php
+++ src/views/layouts/app.blade.php
@@ -11,37 +11,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="base-url" content="{{ url('/') }}">
-
-    <link rel="stylesheet" href="{{ asset('vendor/dashboard/assets/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('vendor/dashboard/assets/bower_components/font-awesome/css/font-awesome.min.css') }}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{ asset('vendor/dashboard/assets/bower_components/Ionicons/css/ionicons.min.css') }}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('vendor/dashboard/assets/dist/css/AdminLTE.min.css') }}">
-    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
-          page. However, you can choose any other skin. Make sure you
-          apply the skin class to the body tag so the changes take effect. -->
-    <link rel="stylesheet" href="{{ asset('vendor/dashboard/assets/dist/css/skins/skin-blue.min.css') }}">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
+  @include('dashboard::layouts.inc.stylesheets')
 
   <!--begin::Global Theme Styles(used by all pages) -->
   <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css"/>
-
-  <!--end::Global Theme Styles -->
-
-
-  <!--begin::Layout Skins(used by all pages) -->
 
   <!--end::Layout Skins -->
   <link rel="shortcut icon" href="{{asset('uploads/project-info/') . '/' . project()->app_icon}}"/>
@@ -100,14 +73,7 @@
 
     @include('dashboard::layouts.partials.footer')
 </div>
-
-<!-- jQuery 3 -->
-<script src="{{ asset('vendor/dashboard/assets/bower_components/jquery/dist/jquery.min.js') }}"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="{{ asset('vendor/dashboard/assets/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('vendor/dashboard/assets/dist/js/adminlte.min.js') }}"></script>
-
+@include('dashboard::layouts.inc.scripts')
 @stack('scripts')
 
 </body>
---
+++ src/views/layouts/inc/scripts.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery 3 -->
+<script src="{{ asset('vendor/dashboard/assets/bower_components/jquery/dist/jquery.min.js') }}"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{{ asset('vendor/dashboard/assets/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('vendor/dashboard/assets/dist/js/adminlte.min.js') }}"></script>
