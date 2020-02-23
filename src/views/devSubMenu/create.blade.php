cffacd15ada489465300a62f999dca88cb9e8d98
Ferdous Anam <ferdous.anam@gmail.com>
2020-02-23 10:47:25 +0600

    Sub Menu CRUD

---
+++ src/views/devSubMenu/create.blade.php
@@ -0,0 +1,109 @@
+@extends('dashboard::layouts.app')
+
+@section('page_title', 'Create Sub Menu')
+@section('page_tagline', 'Create Sub Menu')
+
+@section('content')
+    @include('dashboard::msg.message')
+    <!--begin::Palette-->
+    <div class="box box-default color-palette-box">
+        <div class="box-header with-border">
+            <h3 class="box-title"><i class="fa fa-tag"></i></h3>
+        </div>
+        <!--begin::Form-->
+        <form id="menu-form" action="{{ route('sub-menu.store') }}" method="POST" class="form-horizontal">
+            <div class="box-body">
+                @csrf
+
+                <div class="form-group row">
+                    <label for="serial_no" class="col-sm-2 control-label">Serial No *</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" id="serial_no" name="serial_no"
+                               value="{{ old('serial_no') }}" placeholder="Serial No." required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="parent_id" class="col-sm-2 control-label">Parent Menu *</label>
+                    <div class="col-sm-10">
+                        <select name="parent_id" id="parent_id" class="form-control chosen" required>
+                            <option>Select Parent Menu</option>
+                            @if(isset($main_menus) && count($main_menus)>0)
+                                @foreach ($main_menus as $main_menu)
+                                    <option value="{{$main_menu->id}}">{{$main_menu->menu_name}}</option>
+                                @endforeach
+                            @endif
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="menu_name" class="col-sm-2 control-label">Menu Title *</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" id="menu_name" name="menu_name"
+                               value="{{ old('menu_name') }}" placeholder="Menu Title" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="selector" class="col-sm-2 control-label">Selector *</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" id="selector" name="selector"
+                               value="{{ old('selector') }}" placeholder="Menu ID Selector" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="route_name" class="col-sm-2 control-label">Route Url *</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" id="route_name" name="route_name"
+                               value="{{ old('route_name') }}" placeholder="Route Url" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="icon" class="col-sm-2 control-label">Icon Name</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" id="icon" name="icon" value="{{ old('icon') }}"
+                               placeholder="fa fa-home">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="status" class="col-sm-2 control-label">Status *</label>
+                    <div class="col-sm-10">
+                        <div class="input-active">
+                            <label class="radio">
+                                <input type="radio" name="status" value="1"
+                                    {{(old('status') !== 0) ? 'checked' : ''}}>
+                                Active
+                                <span></span>
+                            </label>
+                            <label class="radio" style="float: left">
+                                <input type="radio" name="status" value="0"
+                                    {{(old('status') === 0) ? 'checked' : ''}}>
+                                Inactive
+                                <span></span>
+                            </label>
+                        </div>
+                        <span class="form-text text-muted"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="box-footer">
+                <div class="row">
+                    <div class="col-sm-2">
+                    </div>
+                    <div class="col-sm-10">
+                        <a href="{{ route('sub-menu.index') }}" class="btn btn-primary">Cancel</a>
+                        <button type="submit" class="btn btn-success">Submit</button>
+                        <button type="reset" class="btn btn-secondary">Reset</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!--end::Portlet-->
+@endsection
+
+@push('scripts')
+    <script>
+        $('#sub-menus-mm').addClass('active menu-open');
+        $('#sub-menus--create-sm').addClass('active');
+    </script>
+@endpush
---
+++ src/views/devSubMenu/index.blade.php
@@ -0,0 +1,41 @@
+@extends('dashboard::layouts.app')
+
+@push('css')
+    <!-- Datatables -->
+    <link rel="stylesheet" href="{{ asset('vendor/dashboard/assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}">
+@endpush
+
+@section('page_title', 'Sub Menu')
+@section('page_tagline', 'Sub Menu List')
+
+@section('content')
+    @include('dashboard::components.delete-modal')
+    @include('dashboard::msg.message')
+    <!--begin::Palette-->
+    <div class="box box-default color-palette-box">
+        <div class="box-header with-border">
+            <h3 class="box-title"><i class="fa fa-tag"></i> Sub Menu List</h3>
+        </div>
+        <div class="box-body">
+            <!--begin: Datatable -->
+            {!! $dataTable->table(['class' => 'table table-striped- table-bordered table-hover table-checkable dataTable no-footer dtr-inline'], true) !!}
+            <!--end: Datatable -->
+        </div>
+    </div>
+    <!--end::Palette-->
+@endsection
+
+@push('scripts')
+    @include('dashboard::scripts.delete')
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sub-menus-mm').addClass('active menu-open');
+            $('#sub-menus-sm').addClass('active');
+        });
+    </script>
+    <!-- Datatables -->
+    <script src="{{ asset('vendor/dashboard/assets/bower_components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendor/dashboard/assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}" type="text/javascript"></script>
+    {!! $dataTable->scripts() !!}
+@endpush
