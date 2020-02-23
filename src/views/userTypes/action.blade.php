e6bf67f7a147c0e489eda0259486d31cb9d4d3d1
Ferdous Anam <ferdous.anam@gmail.com>
2020-02-23 14:02:47 +0600

    User Types CRUD

---
+++ src/views/userTypes/action.blade.php
@@ -0,0 +1,6 @@
+<a href="{{ route('user-type.edit', $id) }}" class="btn btn-sm btn-primary" title="View">
+    <i class="fa fa-edit"></i>
+</a>
+<button type="button" class="btn btn-sm btn-danger delete-button" data-toggle="modal" data-target="#delete-modal" data-id="{{ $id }}">
+    <i class="fa fa-trash lg"></i>
+</button>
---
+++ src/views/userTypes/create.blade.php
@@ -0,0 +1,70 @@
+@extends('dashboard::layouts.app')
+
+@section('page_title', 'Create User Type')
+@section('page_tagline', 'Create User Type')
+
+@section('content')
+    @include('dashboard::msg.message')
+    <!--begin::Palette-->
+    <div class="box box-default color-palette-box">
+        <div class="box-header with-border">
+            <h3 class="box-title"><i class="fa fa-tag"></i></h3>
+        </div>
+        <!--begin::Form-->
+        <form id="menu-form" action="{{ route('user-type.store') }}" method="POST" class="form-horizontal">
+            <div class="box-body">
+                @csrf
+
+                <div class="form-group row">
+                    <label for="priority_name" class="col-sm-2 control-label">User Type:</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" id="priority_name" name="priority_name"
+                               value="{{ old('priority_name') }}" placeholder="User Type" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="priority_description" class="col-sm-2 control-label">Description:</label>
+                    <div class="col-sm-10">
+                        <textarea name="priority_description" id="priority_description" class="form-control" rows="10"
+                                  placeholder="Type Description">{{ old('priority_description') }}</textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="priority_status" class="col-sm-2 control-label">Status:</label>
+                    <div class="col-sm-10">
+                        <div class="radio-inline">
+                            <label class="radio">
+                                <input type="radio" name="priority_status" value="1" checked> Active
+                                <span></span>
+                            </label>
+                            <label class="radio">
+                                <input type="radio" name="priority_status" value="0"> Inactive
+                                <span></span>
+                            </label>
+                        </div>
+                        <span class="form-text text-muted"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="box-footer">
+                <div class="row">
+                    <div class="col-sm-2">
+                    </div>
+                    <div class="col-sm-10">
+                        <a href="{{ route('user-type.index') }}" class="btn btn-primary">Cancel</a>
+                        <button type="submit" class="btn btn-success">Submit</button>
+                        <button type="reset" class="btn btn-secondary">Reset</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+    <!--end::Palette-->
+@endsection
+
+@push('scripts')
+    <script>
+        $('#user-types-mm').addClass('active menu-open');
+        $('#user-types--create-sm').addClass('active');
+    </script>
+@endpush
