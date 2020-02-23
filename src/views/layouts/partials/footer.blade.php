9f631c95a038798ce96655520c18a251d51140a0
Ferdous Anam <ferdous.anam@gmail.com>
2020-02-23 17:47:46 +0600

    Copyright section updated

--- src/views/layouts/partials/footer.blade.php
+++ src/views/layouts/partials/footer.blade.php
@@ -5,5 +5,5 @@
         Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2020 <a href="{{ dev_details()->dev_website }}" target="_blank"> {{ dev_details()->dev_name }}</a>.</strong> All rights reserved.
 </footer>
